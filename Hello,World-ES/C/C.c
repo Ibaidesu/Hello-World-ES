bee5e20dc73c5720df67617074b5670bf88be603
Walkiria007 <112163916+Walkiria2007@users.noreply.github.com>
2022-12-15 21:41:15 +0100

    Add files via upload

---
+++ Hello,World-ES/C/C.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+   // Esto Es  un Hello World Traducido al Español
+   printf("Hola, Mundo!");
+   return 0;
+}
---
+++ Hello,World-ES/C/COBOL.cbl
@@ -0,0 +1,6 @@
+ identification division.
+       program-id. cobol.
+       procedure division.
+       main.
+           display 'Hola Mundo.' end-display.
+           stop run.
